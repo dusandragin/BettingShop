66a65539d7a5fa0fa86489ef4e60117887957d38
Srbo <dragin.dusan@gmail.com>
2018-04-18 23:40:47 +0200

    @T1632 @T1665 registracija zavrsena-popravka

---
+++ KladionicaJPA/src/webServices/KladionicaApplication.java
@@ -0,0 +1,9 @@
+package webServices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class KladionicaApplication extends Application {
+	
+}
